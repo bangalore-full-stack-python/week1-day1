225dff427e8e4498447262e9b4bc984ea8c70ae4
aki2907 <aki.a2907@gmail.com>
2017-06-03 18:26:44 +0530

    fizzbuzz.py

---
+++ mysolutions/fizzbuzz.py
@@ -0,0 +1,14 @@
+"""Author: Akanksha"""
+
+def fizz_buzz(x):
+    for num in range(1, x):
+        if (num % 3 == 0 and num % 5 == 0):
+            print("FizzBuzz")
+        elif (num % 3 == 0):
+            print("Fizz")
+        elif (num % 5 == 0):
+            print("Buzz")
+        else:
+            print(num)
+
+fizz_buzz(20)
